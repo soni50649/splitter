28d592d127a0b4779b423a06744228bb1ec29a26
Your Name <you@example.com>
2021-11-01 14:38:31 +0000

    Made recurring color boards.

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 "use strict"
 
-alert("hello")
+function split4(){
+    addQuarter(this)
+    addQuarter(this)
+    addQuarter(this)
+    addQuarter(this)
+}
+
+
+function addQuarter(intoDiv){
+    let q = document.createElement("div")
+
+    q.style.backgroundColor=`rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})`
+
+    q.classList.add("flexy")
+    q.classList.add("quarter")
+
+    q.addEventListener("click",split4)
+
+    intoDiv.appendChild(q)
+
+}
+
+document.body.addEventListener("click",split4)
